22fa39479bfbb6e0fbad07bfc5c8d915ff461d0d
Jonas Großekathöfer <25958195+grszkthfr@users.noreply.github.com>
2019-11-06 11:00:53 +0100

    Update install.R

--- install.R
+++ install.R
@@ -3,7 +3,6 @@ install.packages("here")
 install.packages("fs")
 install.packages("png")
 install.packages("afex")
-# afex_options(emmeans_model = "multivariate") # use multivariate model for all follow-up tests.
 install.packages("kableExtra")
 install.packages("scales") # for percent_format()
 install.packages("emmeans")
@@ -11,4 +10,3 @@ install.packages("ggplot2")
 install.packages("cowplot")
 install.packages("papaja")
 install.packages("lme4")
-
