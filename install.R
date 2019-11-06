3d05cd92257dda894ac138f6c3b26b814eb6caf2
Jonas Großekathöfer <25958195+grszkthfr@users.noreply.github.com>
2019-11-06 11:00:41 +0100

    Update install.R

--- install.R
+++ install.R
@@ -3,7 +3,7 @@ install.packages("here")
 install.packages("fs")
 install.packages("png")
 install.packages("afex")
-afex_options(emmeans_model = "multivariate") # use multivariate model for all follow-up tests.
+# afex_options(emmeans_model = "multivariate") # use multivariate model for all follow-up tests.
 install.packages("kableExtra")
 install.packages("scales") # for percent_format()
 install.packages("emmeans")
