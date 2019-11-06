946cc9553b1cd117459e31748f27c513be7d0d6b
Jonas Großekathöfer <grszkthfr@outlook.com>
2019-11-06 08:02:23 +0100

    setting up for binder

---
+++ install.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+install.packages("here")
+install.packages("fs")
+install.packages("png")
+install.packages("afex")
+afex_options(emmeans_model = "multivariate") # use multivariate model for all follow-up tests.
+install.packages("kableExtra")
+install.packages("scales") # for percent_format()
+install.packages("emmeans")
+install.packages("ggplot2")
+install.packages("cowplot")
+install.packages("papaja")
+install.packages("lme4")
+
